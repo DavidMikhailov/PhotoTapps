863a5bb1e3b21572e4b94a47db9259332c417dc1
David Mikhailov <david.mikhailovv@gmail.com>
2020-11-24 16:41:59 +0400

    Initial commit

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Давид Михайлов on 17.11.2020.
+//  Copyright © 2020 Давид Михайлов. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    
+}
